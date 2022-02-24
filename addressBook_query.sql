94f300a501dffb1c5efacbffcf9fe3d1e982c201
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-02-24 21:52:30 +0530

    Adding Uc2

--- addressBook_query.sql
+++ addressBook_query.sql
@@ -3,3 +3,25 @@ CREATE DATABASE AddressBook
 
 USE AddressBook
 
+CREATE TABLE AddressBook(
+bookID int IDENTITY PRIMARY KEY,
+book_Name varchar(20)
+)
+
+CREATE TABLE contact_info(
+book_Id INT FOREIGN KEY REFERENCES AddressBook(bookID),
+contact_Id INT IDENTITY PRIMARY KEY,
+firstName VARCHAR(10),
+lastName VARCHAR(15),
+address VARCHAR(50),
+city VARCHAR(15),
+state VARCHAR(15),
+zip VARCHAR(7),
+mobileNo VARCHAR(10),
+email VARCHAR(30)
+)
+
+CREATE TABLE contact_type(
+type_Id INT IDENTITY PRIMARY KEY,
+contact_name VARCHAR (20),
+)
